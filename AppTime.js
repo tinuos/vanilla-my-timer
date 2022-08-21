250f3373f546d911cfe723ad7334c4f8a0c18ab2
tinuos <tinuos91@gmail.com>
2022-08-21 11:41:52 +0900

    dev: 시간 표시 기능 모듈 분할
    - app.js : 앱 전체 스크립트
    - AppTime.js : 앱 시간 표시용 데이터 객체
    - displayAppTime.js : 시간 표시 기능용 스트립트

---
+++ AppTime.js
@@ -0,0 +1,12 @@
+const AppTime = {
+  year: null,
+  month: null,
+  day: null,
+  date: null,
+  hour: null,
+  minute: null,
+  second: null,
+  fullTimeDisplayString: null,
+}
+
+export default AppTime;
---
+++ displayAppTime.js
@@ -0,0 +1,61 @@
+function getCurrentTime() {
+  const time = {};
+  const currentTime = new Date();
+  const week = ['일', '월', '화', '수', '목', '금', '토'];
+  
+  time.year = currentTime.getFullYear();
+  time.month = currentTime.getMonth() === 11 ? 12 : currentTime.getMonth() + 1;
+  time.date = currentTime.getDate();
+  time.day = week[currentTime.getDay()];
+  time.hour = currentTime.getHours();
+  time.minute = currentTime.getMinutes();
+  time.second = currentTime.getSeconds();
+
+  return time;
+}
+
+function setDefaultAppTime(getCurrentTime, AppTime) {
+  const time = getCurrentTime();
+  
+  AppTime.year = time.year;
+  AppTime.month = time.month;
+  AppTime.date = time.date;
+  AppTime.day = time.day;
+  AppTime.hour = time.hour;
+  AppTime.minute = time.minute;
+  AppTime.second = time.second;
+
+  // AppTime 초기 렌더링
+  const y_m_d_d_time_box = document.querySelectorAll('.y-m-d-d-time-box span');
+  const h_m_s_time_box = document.querySelectorAll('.h-m-s-time-box span');
+
+  y_m_d_d_time_box.forEach(element => {
+    element.textContent = AppTime[element.className];
+  });
+  h_m_s_time_box.forEach(element => {
+    element.textContent = AppTime[element.className];
+  }); 
+}
+
+export default function displayAppTime(AppTime) {
+  setDefaultAppTime(getCurrentTime, AppTime);
+
+  setInterval(() => {
+    const currtenTime = getCurrentTime();
+    
+    // 값 비교 후 렌더링
+    for(let currentTimeUnit in currtenTime) {
+      if (AppTime[currentTimeUnit] !== currtenTime[currentTimeUnit]) {
+        AppTime[currentTimeUnit] = currtenTime[currentTimeUnit];
+
+        const targetTimeUnitEl = document.querySelector(`.time-display .${currentTimeUnit}`);
+        
+        if (currentTimeUnit === 'minute' || currentTimeUnit === 'second') {          
+          targetTimeUnitEl.textContent = AppTime[currentTimeUnit].toString().padStart(2, 0);
+        } else {
+          targetTimeUnitEl.textContent = AppTime[currentTimeUnit];
+        }
+      }
+    }
+  }, 1000);
+}
